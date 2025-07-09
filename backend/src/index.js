8d06b097aece4758bd3655c6e2dd31a052314264
Md Saad <mohdsaad2k22@gmail.com>
2025-07-10 01:52:45 +0530

    Project Completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,4 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -8,38 +6,37 @@ import cors from "cors";
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
-import { app,server } from "./lib/socket.js";
 
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT=process.env.PORT;
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.use(express.json({ limit: '10mb' })); // for parsing JSON bodies
-app.use(cookieParser()); // for parsing cookies 
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true, // allow credentials (cookies, authorization headers, etc.) to be sent
-}))
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-//! i made a mistake in the order of the middleware here i put the auth middleware before the body parser middleware ...so first i need to parse the body and then check for the auth token in the body
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
-
-// Serve static files from the frontend build directory
-if(process.env.NODE_ENV === 'production') {
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-// Handle any requests that don't match the above routes 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
-
-server.listen(PORT,()=>{
-    console.log("Server is running on port PORT :",PORT);
-    connectDB();
-})
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-    withCredentials: true,
-})
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  withCredentials: true,
+});
 
 //! http://localhost:5001/api is the base URL for the backend API and this 5001 port is where the backend server is running
 //! axios.create() creates an instance of axios with default settings which means that you can use this instance to make requests without repeating the base URL every time
